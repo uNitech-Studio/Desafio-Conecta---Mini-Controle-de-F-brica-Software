236d8975572d735a69afd25b0cfe0699be7d4f9c
Nicolas Henrique S. Silva <nicolassantossilva2458@gmail.com>
2026-01-21 22:08:21 -0300

    chore: update base for GitHub Pages and rebuild docs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fabrica-software/',
+  base: '/Mini-Controle-Fabrica-Software/',
   build: {
     outDir: 'docs'
   }
