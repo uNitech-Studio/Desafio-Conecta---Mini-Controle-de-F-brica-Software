ecc6ab01685554eaf6d0cd16142824ff8e0c579c
Nicolas Henrique S. Silva <nicolassantossilva2458@gmail.com>
2026-01-30 01:25:29 -0300

    correçao

--- backend/database/migrations/2026_01_25_013759_create_clients_table.php
+++ backend/database/migrations/2026_01_25_013759_create_clients_table.php
@@ -1,8 +1,25 @@
-Schema::create('clients', function (Blueprint $table) {
-    $table->id();
-    $table->string('nome');
-    $table->string('email')->unique();
-    $table->string('telefone')->nullable();
-    $table->boolean('ativo')->default(true);
-    $table->timestamps();
-});
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone')->nullable();
+            $table->boolean('ativo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
